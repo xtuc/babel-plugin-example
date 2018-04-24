b7293c919b8bb5bb6b098478c44d508dcaccc580
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-04-24 16:55:25 +0200

    feat: wrapping

--- plugin.js
+++ plugin.js
@@ -1,8 +1,27 @@
+const template = require("@babel/template").default;
+
+const wrapper = template(`
+  asyncWrapper(FUNCTION)
+`);
+
 module.exports = function ({types: t}) {
   return {
     visitor: {
-      StringLiteral({node}) {
-        node.value = "bar";
+      FunctionDeclaration(path) {
+        const node = path.node;
+
+        if (node.leadingComments) {
+          const [firstComment] = node.leadingComments;
+
+          if (firstComment.value === " @async") {
+            const ast = wrapper({
+              FUNCTION: t.toExpression(node)
+            });
+
+            path.replaceWith(ast);
+            path.stop();
+          }
+        }
       }
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("foo");
+// @async
+function test() {
+  0;
+}
